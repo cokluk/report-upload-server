60c4015c633fc890cdfcc73830d7c08a6a2d806a
s4levelup <47196492+cokluk@users.noreply.github.com>
2024-07-19 13:01:27 +0300

    php version

---
+++ php/inc.php
@@ -0,0 +1,48 @@
+<?php
+
+if($use_domain) { $host = $domain; }
+
+if($ssl) {
+    $host = "https://".$host;
+} else {
+    $host = "http://".$host;
+}
+
+if(isset($_FILES)) {
+    
+    $file = $_FILES['files'];
+ 
+    $target_dir = "public/";
+
+    $target_file = $target_dir . $file['name'][0];
+ 
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+    $tempfilename = "public/".genRandom().".".$imageFileType;
+ 
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" && $imageFileType != "webm" ) {
+        echo "not allowed filetype";
+        $uploadOk = 0;
+    }
+
+    if ($uploadOk == 0) {
+        echo "file not uploaded.";
+    } else {
+        if (move_uploaded_file($file["tmp_name"][0], $tempfilename)) {
+            $response = new stdClass();
+            $response->attachments = array();
+            $response->attachments[0] = new stdClass();
+            $response->attachments[0]->proxy_url = $host."/".$tempfilename;
+            $response->attachments[0]->url =  $host."/".$tempfilename;
+            echo json_encode($response);
+        } else {
+            echo "error uploading your file.";
+        }
+    }
+ 
+}
+ 
+function genRandom() {
+    return "s4-".substr(md5(rand()), 0, 100)."-".rand(1000000000,9999999999);
+}
---
+++ php/index.php
@@ -0,0 +1,15 @@
+<?php 
+ 
+header('Content-Type: application/json');
+
+$host = "127.0.0.1/upload"; // Host name
+
+$use_domain = false;    // Use domain name for the host name
+
+$domain = "example.com/upload"; // Domain name
+
+$ssl = false;   // Use SSL
+
+require_once('inc.php');  // Include the file
+
+?>
